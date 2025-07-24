4597aad9980632473fb7e2dc43f6fce40eee89f3
piyushram612 <kimidipiyushram@gmail.com>
2025-07-24 21:18:48 +0530

    added tailwind.config

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class', // This line enables class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+export default config
