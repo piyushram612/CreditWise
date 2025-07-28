5045da0146888624c0796392c162432a443dbb54
piyushram612 <kimidipiyushram@gmail.com>
2025-07-28 19:46:29 +0530

    changed bank colours

--- app/page.tsx
+++ app/page.tsx
@@ -426,12 +426,12 @@ function MyCardsView({ user, onAddCardClick, onEditCard, onDeleteCard, key }: { 
 
     const getIssuerColorCode = (issuer: string) => {
         switch (issuer?.toLowerCase()) {
-            case 'hdfc': return '#2563EB'; // blue-600
-            case 'sbi': return '#06B6D4'; // cyan-500
-            case 'icici': return '#F97316'; // orange-500
-            case 'axis': return '#9333EA'; // purple-600
+            case 'hdfc': return '#ED232A'; // red
+            case 'sbi': return '#00B5EF'; // cyan-500
+            case 'icici': return ' #F99D27'; // orange-500
+            case 'axis': return '#AE275F'; // purple-600
             case 'amex': return '#1E40AF'; // blue-800
-            case 'idfc': return '#EF4444'; // red-500
+            case 'idfc': return ' #9C1D26'; // red-500
             default: return '#6B7280'; // gray-500
         }
     };
