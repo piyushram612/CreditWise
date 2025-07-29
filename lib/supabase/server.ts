f1cbaacecaa8e3682338b1d0eefca736a0b5ff7d
piyushram612 <kimidipiyushram@gmail.com>
2025-07-29 10:59:04 +0530

    fix: Correct Supabase server client implementation

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,12 @@
-import {
-  createServerActionClient,
-  createServerComponentClient,
-} from '@supabase/auth-helpers-nextjs';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import type { Database } from '../../lib/database.types';
+
+export const createClient = () =>
+  createServerComponentClient<Database>({
+    cookies: cookies,
+  });
+
 
 export function createClient() {
   const cookieStore = cookies()
